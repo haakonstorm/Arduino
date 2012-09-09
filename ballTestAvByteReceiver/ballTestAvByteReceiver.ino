62a7ec4b352a5349ef0a3419446e64cc7ef2f14a
Roger Antonsen <rantonse@1x-193-157-250-154.uio.no>
2012-09-09 13:37:53 +0200

    lagt til

---
+++ ballTestAvByteReceiver/ballTestAvByteReceiver.ino
@@ -0,0 +1,32 @@
+
+#include <Ball2.h>
+#include <MsTimer2.h>
+#include <EEPROM.h>
+
+Ball2 ball;
+byte firstByte;
+byte rB = 0;
+byte gB = 0;
+byte bB = 0;
+
+void setup(){
+  Serial.begin(19200);
+  ball.setColor(0,0,0);
+}
+
+
+void loop(){
+  if(Serial.available() >= 4){
+    firstByte = Serial.read();
+    if (firstByte == 'c') {
+      int rB = Serial.read();
+      int gB = Serial.read();
+      int bB = Serial.read();
+      ball.setColor(rB, gB, bB);
+      Serial.flush();
+    }
+  }
+}
+
+
+
