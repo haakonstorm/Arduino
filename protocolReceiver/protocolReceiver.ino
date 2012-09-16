c1fc06790640c505f9c7c0d79f8e06865035329a
Roger Antonsen <rantonse@polyether.local>
2012-09-16 13:51:12 +0200

    protocol

---
+++ protocolReceiver/protocolReceiver.ino
@@ -0,0 +1,54 @@
+#include <Ball2.h>
+#include <MsTimer2.h>
+#include <EEPROM.h>
+
+Ball2 ball;
+
+boolean active;
+byte input_byte [5];
+byte input_position;
+byte incoming_byte;
+
+void setup(){
+  Serial.begin(19200);
+  ball.setColor(0,0,0);
+}
+
+void loop(){
+}
+
+void runCommand() {
+  if (input_byte[0] == 0) {
+    switch (input_byte[1]) {
+    case 'C':
+      int rB = input_byte[2];
+      int gB = input_byte[3];
+      int bB = input_byte[4];
+      ball.setColor(rB, gB, bB);
+      break;
+    }
+  }
+}
+
+void serialEvent(){
+  while (Serial.available()) {
+    incoming_byte = Serial.read();
+    if (input_position == 5) {
+      active = false;
+      input_position = 0;
+      if (incoming_byte == 3) {
+        runCommand();
+      }
+    } 
+    else if (active == false && incoming_byte == 2) {
+      active = true;
+    } 
+    else if (active == true) {
+      input_byte[input_position] = incoming_byte;
+      input_position++;
+    }
+  }
+}
+
+
+
