e40afcdc10bbe8a895fdceec51991850382007b6
Roger Antonsen <rantonse@polyether.local>
2012-09-19 22:11:34 +0200

    nytt

--- protocolReceiver/protocolReceiver.ino
+++ protocolReceiver/protocolReceiver.ino
@@ -8,7 +8,19 @@ boolean receiving;
 boolean sending;
 byte reportType = 0;
 byte reportNext = 0;
+boolean reportForever = false;
+boolean reportXYZFVP = false;
 byte highByte, lowByte;
+
+int R[256] = {
+  255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,251,245,240,234,228,222,215,209,203,197,192,186,179,173,167,161,155,149,144,138,132,126,120,113,107,101,96,89,83,77,71,65,59,53,48,42,36,30,24,17,11,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,11,17,23,29,35,41,47,53,60,66,72,78,84,90,96,101,107,113,119,125,131,137,143,149,156,162,168,174,180,186,192,197,203,209,215,221,227,233,239,245,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255};
+int G[256] = {
+  0,6,12,18,24,30,36,42,48,54,60,65,72,78,84,89,96,102,108,113,120,126,131,138,144,150,156,162,168,174,179,186,192,198,204,210,216,222,227,234,240,246,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,251,245,240,234,227,221,215,209,203,197,192,186,179,173,167,161,155,149,144,138,131,125,119,113,107,101,96,90,83,77,71,65,59,53,48,42,35,29,23,17,11,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int B[256] = {
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126,132,138,144,150,156,162,168,174,180,186,192,198,204,210,216,222,228,234,240,246,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,251,245,240,234,228,222,216,209,203,197,191,185,179,173,167,161,155,149,144,138,132,126,120,113,107,101,95,89,83,77,71,65,59,53,48,42,36,30,24,17,11,5,0};
+
+
+static int samples = 0;
 
 void setup(){
   Serial.begin(19200);
@@ -24,21 +36,70 @@ void loop(){
 void processAD() {
   ball.processAD();
 
+  if (reportXYZFVP) {
+    if (samples == 50) {
+      samples = 0;
+      Serial.print('m');
+      Serial.print(ball.getX());
+      Serial.print(' ');
+      Serial.print(ball.getY());
+      Serial.print(' ');
+      Serial.print(ball.getZ());
+      Serial.print(' ');
+      Serial.print(ball.getF());
+      Serial.print(' ');
+      Serial.print(ball.getV());
+      Serial.print(' ');
+      Serial.println(ball.getP());
+    }
+    samples++;
+  }
+
+  if (reportForever) {
+    reportNext = 1;
+  }
+
   if (reportNext > 0) {
-    if (reportType == 1) {
+    // Report Thrown.
+    if (reportType == 1 || reportType == 7) {
       if (ball.getThrown()) {
+        randomColor();
         sendThrown();
-        reportNext--;
         ball.resetThrown();
       }
-    }
-    else if (reportType == 2) {
+    } 
+    // Report Landed.
+    else if (reportType == 2 || reportType == 7) {
       if (ball.getLanded()) {
+        ball.setColor(1,1,1);
         sendLanded();
-        reportNext--;
         ball.resetLanded();
       }
     }
+    // Report Tapped.
+    else if (reportType == 3 || reportType == 7) {
+      if (ball.getTapped()) {
+        sendTapped();
+        ball.setColor(0, 50, 0);
+        ball.resetTapped();
+        delay(100);
+        ball.setColor(0, 0, 0);
+        ball.resetTapped();
+      }
+    }
+    // Report Force.
+    else if (reportType == 4) {
+      sendForce();
+    }
+    // Report Velocity.
+    else if (reportType == 5) {
+      sendVelocity();
+    }
+    // Report XYZFVP.
+    else if (reportType == 6) {
+      reportXYZFVP = true;
+    }
+    reportNext--;
   }
 }
 
@@ -50,6 +111,8 @@ void processAD() {
 // 2: Command.
 //    C: color.
 //       3-5: R,G,B
+//    D: direct color.
+//       3: value (0-255)
 //    T: tone.
 //       3-5: Higbyte for frequency, Lowbyte for frequency, Duration in 1/100 second.
 //    I: info.
@@ -63,8 +126,13 @@ void processAD() {
 //       3: Type
 //          1: means Thrown
 //          2: means Landed
+//          3: means Tapped
+//          4: means Force
+//          5: means Velocity
+//          6: means XYZFVP
+//          7: means Thrown, Landed, Tapped
 //       4: n
-//       5: unspecified
+//       5: 1 = report forever / 0 = stop
 
 void runCommand() {
   // If first byte is 0, it applies to all balls. Otherwise, first byte must equal the Id.
@@ -77,6 +145,13 @@ void runCommand() {
         int gB = input_byte[3];
         int bB = input_byte[4];
         ball.setColor(rB, gB, bB);
+      }
+      break;
+      // Set direct color.
+    case 'D': 
+      {
+        int value = input_byte[2];
+        setColor(value);
       }
       break;
       // Set tone.
@@ -128,8 +203,20 @@ void runCommand() {
       {
         ball.resetThrown();
         ball.resetLanded();
+        ball.resetTapped();
         reportType = input_byte[2];
         reportNext = input_byte[3];
+        // If the last byte is 0, stop everything.
+        if (input_byte[4] == 0) {
+          reportForever = false;
+          reportXYZFVP = false;
+          reportNext = 0;
+          reportType = 0;
+        } 
+        else if (input_byte[4] == 1) {
+          reportForever = true;
+          reportNext = 0;
+        }
       }
       break;
     }
@@ -154,10 +241,10 @@ void sendThrown() {
   lowByte = (byte) (ball.getHoldTime() - (highByte * 256));
   Serial.write(4);
   Serial.write(ball.getId()); // 1. ID
-  Serial.write('T'); // 2. Thrown
-  Serial.write(highByte); // 3. Higbyte for number of samples
-  Serial.write(lowByte); // 4. Lowbyte for number of samples
-  Serial.write((byte) 0); // 5. Unspecified.
+  Serial.write('R'); // 2. Report
+  Serial.write('T'); // 3. Thrown
+  Serial.write(highByte); // 4. Higbyte for number of samples
+  Serial.write(lowByte); // 5. Lowbyte for number of samples
   Serial.write(5);
   sending = false;
 }
@@ -168,9 +255,47 @@ void sendLanded() {
   lowByte = (byte) (ball.getFlyTime() - (highByte * 256));
   Serial.write(4);
   Serial.write(ball.getId()); // 1. ID
-  Serial.write('L'); // 2. Landed
-  Serial.write(highByte); // 3. Higbyte for number of samples
-  Serial.write(lowByte); // 4. Lowbyte for number of samples
+  Serial.write('R'); // 2. Report
+  Serial.write('L'); // 3. Landed
+  Serial.write(highByte); // 4. Higbyte for number of samples
+  Serial.write(lowByte); // 5. Lowbyte for number of samples
+  Serial.write(5);
+  sending = false;
+}
+
+void sendTapped() {
+  sending = true;
+  Serial.write(4);
+  Serial.write(ball.getId()); // 1. ID
+  Serial.write('R'); // 2. Report
+  Serial.write('P'); // 3. Tapped
+  Serial.write((byte) 0); // 4. Unspecified.
+  Serial.write((byte) 0); // 5. Unspecified.
+  Serial.write(5);
+  sending = false;
+}
+
+// Dummy function
+void sendForce() {
+  sending = true;
+  Serial.write(4);
+  Serial.write(ball.getId()); // 1. ID
+  Serial.write('R'); // 2. Report
+  Serial.write('F'); // 3. Force
+  Serial.write((byte) 0); // 4. Unspecified.
+  Serial.write((byte) 0); // 5. Unspecified.
+  Serial.write(5);
+  sending = false;
+}
+
+// Dummy function
+void sendVelocity() {
+  sending = true;
+  Serial.write(4);
+  Serial.write(ball.getId()); // 1. ID
+  Serial.write('R'); // 2. Report
+  Serial.write('V'); // 3. Velocity
+  Serial.write((byte) 0); // 4. Unspecified.
   Serial.write((byte) 0); // 5. Unspecified.
   Serial.write(5);
   sending = false;
@@ -234,6 +359,22 @@ void reportNotOK() {
   delay(200);
   ball.setColor(0,0,0);
 }
+
+void setColor(byte i){
+  ball.setColor(R[i], G[i], B[i]);
+}
+
+void randomColor() {
+  setColor((int) random (256));
+}
+
+
+
+
+
+
+
+
 
 
 
