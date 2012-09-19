8d90bb030d2a6c5b565f6133e7bc944c00e2ac45
Roger Antonsen <rantonse@1x-193-157-245-115.uio.no>
2012-09-19 17:20:34 +0200

    multipurpose receiver

--- protocolReceiver/protocolReceiver.ino
+++ protocolReceiver/protocolReceiver.ino
@@ -6,14 +6,40 @@ Ball2 ball;
 byte input_byte [5];
 boolean receiving;
 boolean sending;
+byte reportType = 0;
+byte reportNext = 0;
+byte highByte, lowByte;
 
 void setup(){
   Serial.begin(19200);
+  sendInfo();
   ball.setColor(0,0,0);
-  sendId()
+  MsTimer2::set(5, processAD);
+  MsTimer2::start();
 }
 
 void loop(){
+}
+
+void processAD() {
+  ball.processAD();
+
+  if (reportNext > 0) {
+    if (reportType == 1) {
+      if (ball.getThrown()) {
+        sendThrown();
+        reportNext--;
+        ball.resetThrown();
+      }
+    }
+    else if (reportType == 2) {
+      if (ball.getLanded()) {
+        sendLanded();
+        reportNext--;
+        ball.resetLanded();
+      }
+    }
+  }
 }
 
 // Communication protocol.
@@ -26,9 +52,19 @@ void loop(){
 //       3-5: R,G,B
 //    T: tone.
 //       3-5: Higbyte for frequency, Lowbyte for frequency, Duration in 1/100 second.
-//    M: message.
-//       3-5: Type
-//            I,I,I: Id
+//    I: info.
+//       3-5: 0,0,0: Id
+//    E: Write to EEPROM.
+//       3 and 5: Type
+//          6: ID
+//          7: SPEAKER
+//       4: Value
+//    R: Report next n instances of type.
+//       3: Type
+//          1: means Thrown
+//          2: means Landed
+//       4: n
+//       5: unspecified
 
 void runCommand() {
   // If first byte is 0, it applies to all balls. Otherwise, first byte must equal the Id.
@@ -46,28 +82,96 @@ void runCommand() {
       // Set tone.
     case 'T':
       {
-        int frequency = input_byte[2] * 265 + input_byte[3];
+        int frequency = input_byte[2] * 256 + input_byte[3];
         int duration = input_byte[4] * 10;
         tone(2, frequency, duration);
       }
       break;
-    case 'M':
+      // Send info.
+    case 'I':
       {
-        if (input_byte[2]  == 'I' && input_byte[3]  == 'I' && input_byte[4]  == 'I') {
-          sendId();
+        // If all values are 0, send info.
+        if (input_byte[2]  == 0 && input_byte[3]  == 0 && input_byte[4]  == 0) {
+          sendInfo();
         }
+      }
+      break;
+    case 'E':
+      {
+        // If value 3 and 5 are 6, then write ID to EEPROM.
+        if (input_byte[2]  == 6 && input_byte[4]  == 6) {
+          EEPROM.write(ID,input_byte[3]);
+          if(input_byte[3] == EEPROM.read(ID)) {
+            reportOK();
+          } 
+          else {
+            reportNotOK();
+          }
+          sendInfo();
+        } 
+        else 
+          // If value 3 and 5 are 6, then write SPEAKER to EEPROM.
+        if (input_byte[2]  == 7 && input_byte[4]  == 7) {
+          EEPROM.write(SPEAKER,input_byte[3]);
+          if(input_byte[3] == EEPROM.read(SPEAKER)) {
+            reportOK();
+          } 
+          else {
+            reportNotOK();
+          }
+          sendInfo();
+        }
+      }
+      break;
+      // Set report type and number
+    case 'R': 
+      {
+        ball.resetThrown();
+        ball.resetLanded();
+        reportType = input_byte[2];
+        reportNext = input_byte[3];
       }
       break;
     }
   }
 }
 
-void sendId() {
+void sendInfo() {
   sending = true;
   Serial.write(4);
-  Serial.write(ball.getId());
-  Serial.write(ball.getId());
-  Serial.write(ball.getId());
+  Serial.write(ball.getId());  
+  Serial.write('I');
+  Serial.write(ball.hasSpeaker());
+  Serial.write((byte) ((ball.getBatteryLevel() - 2.0) * 100));
+  Serial.write(ball.getInAir());
+  Serial.write(5);
+  sending = false;
+}
+
+void sendThrown() {
+  sending = true;
+  highByte = (byte) (ball.getHoldTime() / 256);
+  lowByte = (byte) (ball.getHoldTime() - (highByte * 256));
+  Serial.write(4);
+  Serial.write(ball.getId()); // 1. ID
+  Serial.write('T'); // 2. Thrown
+  Serial.write(highByte); // 3. Higbyte for number of samples
+  Serial.write(lowByte); // 4. Lowbyte for number of samples
+  Serial.write((byte) 0); // 5. Unspecified.
+  Serial.write(5);
+  sending = false;
+}
+
+void sendLanded() {
+  sending = true;
+  highByte = (byte) (ball.getFlyTime() / 256);
+  lowByte = (byte) (ball.getFlyTime() - (highByte * 256));
+  Serial.write(4);
+  Serial.write(ball.getId()); // 1. ID
+  Serial.write('L'); // 2. Landed
+  Serial.write(highByte); // 3. Higbyte for number of samples
+  Serial.write(lowByte); // 4. Lowbyte for number of samples
+  Serial.write((byte) 0); // 5. Unspecified.
   Serial.write(5);
   sending = false;
 }
@@ -77,10 +181,10 @@ void serialEvent(){
   static byte incoming_byte;
 
   while (!sending && Serial.available()) {
+    // Read one byte.
     incoming_byte = Serial.read();
-
     // If we have read five bytes.
-    if (input_position == 5) {
+    if (receiving && input_position == 5) {
       // We are not receiving anymore.
       receiving = false;
       // Reset counter and get ready for next message.
@@ -92,19 +196,44 @@ void serialEvent(){
       }
     }
     // If we are not receiving and the byte is 2, then start receiving.
-    else if (receiving == false && incoming_byte == 2) {
+    else if (!receiving && incoming_byte == 2) {
       receiving = true;
     }
     // If we are listening, read byte (until input_position is 5).
-    else if (receiving == true) {
+    else if (receiving) {
       input_byte[input_position] = incoming_byte;
       input_position++;
     }
   }
 }
 
+void reportOK() {
+  ball.setColor(0,255,0);
+  delay(200);
+  ball.setColor(0,0,0);
+  delay(200);
+  ball.setColor(0,255,0);
+  delay(200);
+  ball.setColor(0,0,0);
+  delay(200);
+  ball.setColor(0,255,0);
+  delay(200);
+  ball.setColor(0,0,0);
+}
 
-
+void reportNotOK() {
+  ball.setColor(255,0,0);
+  delay(200);
+  ball.setColor(0,0,0);
+  delay(200);
+  ball.setColor(255,0,0);
+  delay(200);
+  ball.setColor(0,0,0);
+  delay(200);
+  ball.setColor(255,0,0);
+  delay(200);
+  ball.setColor(0,0,0);
+}
 
 
 
