da5af69c8475c6b18667f9ea909bf089d150b622
Roger Antonsen <rantonse@polyether.local>
2012-09-28 07:33:12 -0400

    force integrals, fading, a lot

--- libraries/Ball2/Ball2.cpp
+++ libraries/Ball2/Ball2.cpp
@@ -5,6 +5,11 @@
 
 #include <Ball2.h>
 
+
+const byte Ball2::_simpleColorR[256] = {255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,251,245,240,234,228,222,215,209,203,197,192,186,179,173,167,161,155,149,144,138,132,126,120,113,107,101,96,89,83,77,71,65,59,53,48,42,36,30,24,17,11,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,11,17,23,29,35,41,47,53,60,66,72,78,84,90,96,101,107,113,119,125,131,137,143,149,156,162,168,174,180,186,192,197,203,209,215,221,227,233,239,245,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255};
+const byte Ball2::_simpleColorG[256] = {0,6,12,18,24,30,36,42,48,54,60,65,72,78,84,89,96,102,108,113,120,126,131,138,144,150,156,162,168,174,179,186,192,198,204,210,216,222,227,234,240,246,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,251,245,240,234,227,221,215,209,203,197,192,186,179,173,167,161,155,149,144,138,131,125,119,113,107,101,96,90,83,77,71,65,59,53,48,42,35,29,23,17,11,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+const byte Ball2::_simpleColorB[256] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126,132,138,144,150,156,162,168,174,180,186,192,198,204,210,216,222,228,234,240,246,252,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,251,245,240,234,228,222,216,209,203,197,191,185,179,173,167,161,155,149,144,138,132,126,120,113,107,101,95,89,83,77,71,65,59,53,48,42,36,30,24,17,11,5,0};
+
 // Constructor for Ball
 Ball2::Ball2() {
   	pinMode(BLUEPIN, OUTPUT);
@@ -29,10 +34,17 @@ Ball2::Ball2() {
 	_V = 0.0;
 	_P = 0.0;
 
+	// Force history
+	_forceHistoryPointer = 0;
+	_lastFreeFallPointer = 0;
+
+	_brightness = 255;
+	_targetBrightness = 255;
+	_fadeSpeed = 1;
 }
 
 void Ball2::processAD(void) {
-  	static byte count;
+  	static int count;
   	static int identicalFs = 0;
   	static long longX, longY, longZ;
 
@@ -53,15 +65,22 @@ void Ball2::processAD(void) {
 	// Calculate the force acting on the ball in this timestep.
 	_F = (int) sqrt (longX * longX + longY * longY + longZ * longZ);
 
+	// Force history
+	_forceHistory [_forceHistoryPointer] = _F - _oneG;
+	_forceHistoryPointer++;
+	if (_forceHistoryPointer >= 100) {
+		_forceHistoryPointer = 0;
+	}
+
 	// Calculate the difference in force.
 	_diffF = abs(_F - _prevF);
 
 	// Auto-calibration of _oneG and reset velocity.
 	if (abs(102.0 - _F) < 4) {
 		identicalFs++;
-		// If we have read 60 identical measurements, reset and calibrate.
+		// If we have read 30 identical measurements, reset and calibrate.
 		if ((identicalFs >= 30)) {
-			_oneG = _F;
+			// _oneG = _F;
 			_V = 0.0;
 			// _P = 0.0; Leave _P untouched, because it
 			// does not make sense to reset it.
@@ -108,6 +127,7 @@ void Ball2::processAD(void) {
 		_landed = TRUE;
 		_flyTime = count;
 		count = 0;
+		_lastFreeFallPointer = _forceHistoryPointer;
 	}
 
 	// Try to determine if the ball has been tapped.
@@ -115,6 +135,21 @@ void Ball2::processAD(void) {
   		_tapped = TRUE;
 		count = 0;
   	}
+
+	if (_brightness != _targetBrightness) {
+		if (_brightness < _targetBrightness) {
+			_brightness = _brightness + _fadeSpeed;
+			if (_brightness > _targetBrightness) {
+				_brightness = _targetBrightness;
+			}
+		} else {
+			_brightness = _brightness - _fadeSpeed;
+			if (_brightness < _targetBrightness) {
+				_brightness = _targetBrightness;
+			}
+		}
+		setSimpleColor(_simpleColor);
+	}
 }
 
 //The function aboves takes some time to execute. In the cases where
@@ -158,16 +193,57 @@ void Ball2::setColor(byte R, byte G, byte B) {
 	analogWrite(BLUEPIN, B);
 }
 
-//	Returns the force acting on the ball in the x-direction. Used in physics experiments.
+// Sets the color of the ball. Simple color C is input.
+void Ball2::setSimpleColor(int C) {
+	_simpleColor = C;
+	analogWrite(REDPIN, _simpleColorR[C] * _brightness / 256);
+	analogWrite(GREENPIN, _simpleColorG[C] * _brightness / 256);
+	analogWrite(BLUEPIN, _simpleColorB[C] * _brightness / 256);
+}
+
+// Sets the SiteSwap color of the ball. SiteSwap number N is input.
+void Ball2::setSiteSwapColor(byte N) {
+	switch (N) {
+	case 0:
+		setColor(1,1,1); // DIM
+		break;
+	case 1:
+		setSimpleColor(0); // RED
+		break;
+	case 2:
+		setSimpleColor(42); // RED
+		break;
+	case 3:
+		setSimpleColor(85); // GREEN
+		break;
+	case 4:
+		setSimpleColor(171); // BLUE
+		break;
+	case 5:
+		setSimpleColor(215); // PURPLE
+		break;
+	case 6:
+		setSimpleColor(128); // CYAN ? (GREEN + BLUE)
+		break;
+	default:
+		setColor(1,1,1); // DIM
+		break;
+	}
+}
+
+// Returns the force acting on the ball in the x-direction. Used in
+// physics experiments.
 int Ball2::getX() {
 	return _x;
 }
 
-// Returns the force acting on the ball in the y-direction. Used in physics experiments.
+// Returns the force acting on the ball in the y-direction. Used in
+// physics experiments.
 int Ball2::getY() {
 	return _y;
 }
-// Returns the force acting on the ball in the z-direction. Used in physics experiments.
+// Returns the force acting on the ball in the z-direction. Used in
+// physics experiments.
 int Ball2::getZ() {
 	return _z;
 }
@@ -262,3 +338,41 @@ float Ball2::getBatteryLevel() {
 int Ball2::getId() {
 	return _id;
 }
+
+// Force integral. Ignore first 5 values after landing.
+
+int Ball2::getForceIntegral() {
+	int forceIntegral = 0;
+	_lastFreeFallPointer = (_lastFreeFallPointer + 5) % 100;
+	if (_lastFreeFallPointer < _forceHistoryPointer) {
+		for (int i = _lastFreeFallPointer; i < _forceHistoryPointer; i++) {
+			forceIntegral += _forceHistory[i];
+		}
+	} else {
+		for (int i = _lastFreeFallPointer; i < 100; i++) {
+			forceIntegral += _forceHistory[i];
+		}
+		for (int i = 0; i < _forceHistoryPointer; i++) {
+			forceIntegral += _forceHistory[i];
+		}
+	}
+	return forceIntegral;
+}
+
+void Ball2::setBrightness(byte I) {
+	_brightness = I;
+	setSimpleColor(_simpleColor);
+}
+
+byte Ball2::getBrightness() {
+	return _brightness;
+}
+
+void Ball2::setTargetBrightness(byte I) {
+	_targetBrightness = I;
+}
+
+void Ball2::setFadeSpeed(byte I) {
+	_fadeSpeed = I;
+}
+
