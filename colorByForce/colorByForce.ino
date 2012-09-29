df3f377b796f61e3a232606f344f7dc3394473cc
Roger Antonsen <rantonse@polyether.local>
2012-09-28 23:36:51 -0400

    more sophisticated

--- colorByForce/colorByForce.ino
+++ colorByForce/colorByForce.ino
@@ -4,10 +4,16 @@
 #include <EEPROM.h>
 
 Ball2 ball;
+int force, simpleColor;
 
 void setup(){
+  Serial.begin(19200);
+  checkBattery();
   MsTimer2::set(5, processAD);
   MsTimer2::start();
+}
+
+void loop() {
 }
 
 void processAD(){
@@ -15,6 +21,7 @@ void processAD(){
 
   if(ball.getLanded()){
     ball.resetLanded();
+    sendCommand(0, 'F', 0, 1, 1);
     ball.setTargetBrightness(1);
     ball.setFadeSpeed(1);
   }
@@ -22,14 +29,52 @@ void processAD(){
   if(ball.getThrown()){
     ball.resetThrown();
     
-    int force = ball.getForceIntegral();
-    int value = map(constrain(force, 6000, 12000),6000,12000,150,255);
+    force = ball.getForceIntegral();
+    simpleColor = map(constrain(force, 6000, 12000), 6000, 12000, 150, 255);
     
-    ball.setSimpleColor((byte) value);
+    sendCommand(0, 'S', simpleColor, 0, 0);
+    sendCommand(0, 'F', 0, 255, 7);
+    sendCommand(0, 'F', 0, 255, 7);
+    ball.setSimpleColor((byte) simpleColor);
     ball.setTargetBrightness(255);
     ball.setFadeSpeed(7);
   }
 }
 
-void loop() {
+void checkBattery() {
+  int flashes;
+  float batteryLevel = ball.getBatteryLevel();
+  if(batteryLevel>4.0f)
+    flashes=6;
+  else if (batteryLevel>3.95f)
+    flashes=5;
+  else if (batteryLevel>3.90f)
+    flashes=4;
+  else if (batteryLevel>3.80f)
+    flashes=3;
+  else if (batteryLevel>3.70f)
+    flashes=2;
+  else
+    flashes=1;
+
+  for(int i = 0;i<flashes;i++){
+    ball.setColor(0,10,0);
+    delay(50);
+    ball.setColor(0,0,0);
+    delay(100);
+  }  
 }
+
+void sendCommand(byte A, byte B, byte C, byte D, byte E) {
+  Serial.write(2);
+  Serial.write(A);
+  Serial.write(B);
+  Serial.write(C);
+  Serial.write(D);
+  Serial.write(E);
+  Serial.write(3);
+}
+
+
+
+
